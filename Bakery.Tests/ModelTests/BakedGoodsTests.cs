afaeff7280896a6c1295c0d2f7065bcee8612348
Cristina Plesa <cristinaplesa8@gmail.com>
2021-05-07 11:46:33 -0700

    BUGFIX: refactor code in cs and test files to include Get method, Red Phase passed

--- Bakery.Tests/ModelTests/BakedGoodsTests.cs
+++ Bakery.Tests/ModelTests/BakedGoodsTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System;
 using Bakery.Models;
 
@@ -11,26 +12,27 @@ namespace Bakery.Test
     public void BreadClass_CreatesInstanceOfBread_Bread()
     {
       // Arrange/Act
-      Bread newBread = new Bread("test", "test", 0);
+      Bread newBread = new Bread("test", "test", 1);
       // Assert
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
       // Arrange
-      string breadName = "Colac";
-      string description = "Traditional Romanian sweet bread. It is made from whole wheat flour and is braided in a circle. Traditionally this bread is made for special occasions, but we make it fresh daily!";
-      int price = 2;
-      Bread newBread = new Bread(breadName, description, price);
+      string testBreadName = "Colac";
+      string testDescription = "Traditional Romanian sweet bread. It is made from whole wheat flour and is braided in a circle. Traditionally this bread is made for special occasions, but we make it fresh daily!";
+      int testPrice = 2;
+      Bread newBread = new Bread(testBreadName, testDescription, testPrice);
 
       //Act
-      string result = newBread.breadName;
+      string expected = newBread.BreadName;
       // string actualResult = newBread.Bread(breadName, description, price);
       // string anticipatedResult = breadName, description, price;
 
       // Assert
-      Assert.AreEqual(result, breadName);
+      Assert.AreEqual(result, testBreadName);
     }
     // Tests
   }
--- Bakery/Models/BakedGoods.cs
+++ Bakery/Models/BakedGoods.cs
@@ -1,15 +1,19 @@
+using System.Collections.Generic;
 using System;
-using Bakery.Models;
 
 namespace Bakery.Models
 {
   public class Bread
   {
-    public Bread(string breadName, string description, int price)
-    {
-      return;
-    }
-    // Code for constructor would go here, but should I forgo the constructor and just use two classes?
+  public string BreadName { get; }
+  public string Description { get; }
+  public int Price { get; }
+
+  public Bread(string breadName, string description, int price)
+  {
+    return;
+  }
+  // Code for constructor would go here, but should I forgo the constructor and just use two classes?
   }
 
   // public class Pastry. Do the same for this class, create constructor, then create a few new object instances of different pastries. Must test bread one first tho. Will it work to have two classes in the same file?
